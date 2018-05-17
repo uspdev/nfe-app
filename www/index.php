d0142b14ca9b0d50b902f693787fac8b79b02d0e
Masaki <masaki.kawabata@gmail.com>
2018-05-17 14:39:50 -0300

    melhorias esteticas

--- www/index.php
+++ www/index.php
@@ -27,67 +27,79 @@ include '../config.php';
 <div ng-controller="main">
 
     <div class="container-fluid">
-        <form name="Form" ng-submit="submit()">
+        <div class="row">
+            <div class="col-md-12">
 
-            <p>Webservice</p>
-            <input type="text" ng-model="url" size="50" name="url"/><br><br>
 
-            <p>Cole o xml aqui</p>
-            <textarea name="xml" ng-model="xml" rows="10" cols="50"></textarea>
+                <form name="Form" ng-submit="submit()">
 
-            <p>Ou a chave (somente numeros - 44 digitos)</p>
-            <input type="text" ng-model="chave" size="50" name="chave"/>
-            <br/><br/>
-            <input type="submit" value="Enviar">
-        </form>
+                    <p>Webservice</p>
+                    <input type="text" ng-model="url" size="50" name="url"/><br><br>
 
-        <div ng-view ng-show="res">
-            <br/>
-            <b>Retorno</b><br/>
+                    <p>Cole o xml aqui</p>
+                    <textarea name="xml" ng-model="xml" rows="6" cols="100"></textarea>
 
-            <div ng-show="res.chave">
-                Chave: <b>{{res.chave}}</b>
+                    <p>Ou a chave (somente numeros - 44 digitos)</p>
+                    <input type="text" ng-model="chave" size="50" name="chave"/>
+                    <br/><br/>
+                    <input type="submit" value="Enviar">
+                </form>
             </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
 
-            <div ng-show="res.xml">
-                <br/>
-                <div><b>XML</b></div>
-                <div ng-repeat="(key,val) in res.xml">
-                    {{key}}: <span ng-class="{'red':erro(val)}"><b>{{val}}</b></span>
-                </div>
-            </div>
+                <div ng-view ng-show="res">
+                    <br/>
+                    <b>Retorno</b><br/>
 
-            <div ng-show="res.prot">
-                <br/>
-                <b>Protocolo</b><br/>
-                <div ng-repeat="(key,val) in res.prot" ng-if="key != 'raw' && key != 'eventos' ">
+                    <div ng-show="res.chave">
+                        Chave: <b>{{res.chave}}</b>
+                    </div>
+
+                    <div ng-show="res.xml">
+                        <br/>
+                        <div><b>XML</b></div>
+                        <div ng-repeat="(key,val) in res.xml">
+                            {{key}}: <span ng-class="{'red':erro(val)}"><b>{{val}}</b></span>
+                        </div>
+                    </div>
+
+                    <div ng-show="res.prot">
+                        <br/>
+                        <b>Protocolo</b><br/>
+                        <div ng-repeat="(key,val) in res.prot" ng-if="key != 'raw' && key != 'eventos' ">
                     <span>{{key}}: <span ng-class="{'red':erro(val)}"><b>{{val}}</b></span>
-                </div>
+                        </div>
 
-                <div ng-repeat="ev in res.prot.eventos">
-                    Evento: ({{ ev.dhEvento }}) {{ ev.tpEvento }} - {{ ev.descEvento }}
-                </div>
-            </div>
-
-            <div ng-show="res.sefaz">
-                <br/>
-                <div><b>SEFAZ</b></div>
-                <div ng-repeat="(key,val) in res.sefaz">
-                    {{key}}: <span ng-class="{'red':erro(val)}"><b>{{val}}</b></span>
+                        <div ng-repeat="ev in res.prot.eventos">
+                            Evento: ({{ ev.dhEvento }}) {{ ev.tpEvento }} - {{ ev.descEvento }}
+                        </div>
+                    </div>
                 </div>
             </div>
+            <div class="col-md-6">
 
-            <div ng-show="res.url.length != 0">
-                <br/>
-                <b>Links</b>
-                <div ng-repeat="(key,val) in res.url">
-                    {{key}}: <a href="{{val}}">{{key}}</a>
+                <div ng-show="res.sefaz">
+                    <br/>
+                    <div><b>SEFAZ</b></div>
+                    <div ng-repeat="(key,val) in res.sefaz">
+                        {{key}}: <span ng-class="{'red':erro(val)}"><b>{{val}}</b></span>
+                    </div>
+                </div>
+
+                <div ng-show="res.url">
+                    <br/>
+                    <div><b>Links</b></div>
+                    <div ng-repeat="(key,val) in res.url">
+                        {{key}}: <a href="{{val}}">{{key}}</a>
+                    </div>
                 </div>
             </div>
-
         </div>
     </div>
 </div>
+
 
 <script src="lib/jquery-2.1.1.min.js"></script>
 <script src="components/angular/angular.js"></script>
@@ -116,8 +128,6 @@ include '../config.php';
                     console.log('entrou');
                 }
             }
-
-
         };
 
 
